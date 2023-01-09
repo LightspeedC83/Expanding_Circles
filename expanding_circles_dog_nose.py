1eef4cd34070d1b3657ed7cdbae5f396cc1b6303
Chase Lowney <chasea2016@gmail.com>
2023-01-09 09:40:53 -0500

    expanding circle + animation capability

--- expanding_circles_dog_nose.py
+++ expanding_circles_dog_nose.py
@@ -1,15 +1,30 @@
 from PIL import Image
 import math
+import moviepy.editor as me
+import os
 
-#deciding what cooridinates need to be drawn
+
+frame_output_folder = "output_frames"
+# clearing the frame_output_folder
+for file in os.listdir(frame_output_folder):
+    file = frame_output_folder + "/" + file
+    os.remove(file)
+
 
 size = (1001,1001) #setting the size for the image to be created
 pixel_list = []
 
-for x in range(0,size[0]*size[1]): #generating all white values to populate the pixel list
-    pixel_list.append((255,255,255))
+def clear_pixel_list():
+    """populates the pixel_list list with all white pixel values according to the specified size of the image"""
+    global pixel_list
+    pixel_list = []
+    for x in range(0,size[0]*size[1]): #generating all white values to populate the pixel list
+        pixel_list.append((255,255,255))
 
-#first finding the center pixel of the grid 
+clear_pixel_list() #creating an initial blank image
+
+# getting horizontal and vertical offset from the center of the image to the top left 
+# (used in converting coordinate to an element in the pixel list) 
 horizontal_offset = size[0]//2 #for if (0,0) was at the top left
 vertical_offset = size[1]//2
 
@@ -18,26 +33,33 @@ def coordinate_to_element(x,y):
     and outputs the element in the list of pixels in the image that said coordinate pair corresponds to
     (ie. it takes ordered pair and converts it to a pixel on the image)"""
 
-    return (x + horizontal_offset) + (((vertical_offset - y)) *  size[1]) #something is fucking up when y<0
+    return (x + horizontal_offset) + ((vertical_offset - y) *  size[1]) 
 
-#testing out coordinate system with simple line
-# for x in range(-100,101):
-#     pixel_list[coordinate_to_element(x,x)] = (0,0,0)
+# This makes a circle that expands
+images = [] 
+radius = 0
+for frame in range(0, 100):
+    radius +=1
+    for x in range(-100,101):
+        radicand = (radius**2) - (x**2)
+        if radicand >=0:
+            y = math.sqrt(radicand)
+            y_closest = round(y)
+            
+            pixel_list[coordinate_to_element(x,y_closest)] = (0,0,0)
+            pixel_list[coordinate_to_element(x,-1*y_closest)] = (0,0,0) 
 
-# This makes a circle  
-radius = 100
-for x in range(-100,101):
-    radicand = (radius**2) - (x**2)
-    if radicand >=0:
-        y = math.sqrt(radicand)
-        y_closest = round(y)
-        
-        pixel_list[coordinate_to_element(x,y_closest)] = (0,0,0)
-        pixel_list[coordinate_to_element(x,-1*y_closest)] = (0,0,0) # (this sideways parabola definitely shows that it's doing a sqrt function and not circle as intended)
+    #drawing those coordinates to the image and saving the frame
+    output = Image.new(mode="RGB", size=size)
 
-#drawing those coordinates and saving the frame
-output = Image.new(mode="RGB", size=size)
+    output.putdata(pixel_list)
 
-output.putdata(pixel_list)
+    output.save(f"{frame_output_folder}/frame{frame}.jpg")
 
-output.show()
+    # clear_pixel_list()
+
+# saving all the frames as a video
+frames = [frame_output_folder+"/"+f for f in os.listdir(frame_output_folder)] #getting a list of all the frame file paths
+
+output_clip = me.ImageSequenceClip(frames, fps=24)
+output_clip.write_videofile("ouput_animation.mp4", fps=24)  # is producing video that is a bit spotty, I think duplicate frames or frames that are out of order in output_frames is the culprit
