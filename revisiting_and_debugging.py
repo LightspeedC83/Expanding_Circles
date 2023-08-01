cf742cf9a6406f9ec7ec92ccb6eef5fb3e720676
Chase Lowney <chasea2016@gmail.com>
2023-08-01 00:24:33 -0400

    testing gitignore

--- revisiting_and_debugging.py
+++ revisiting_and_debugging.py
@@ -12,7 +12,7 @@ for file in os.listdir(frame_output_folder):
     os.remove(file)
 
 
-size = (5, 5) #setting the size for the image to be created
+size = (10, 10) #setting the size for the image to be created
 pixel_list = []
 
 # function to clear the pixel list
